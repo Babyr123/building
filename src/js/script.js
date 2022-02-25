638b1772f10f8c57c3f16cff34951782ef58f810
Babyr123 <babyr.2010@mail.rugit config --global user.email babyr.2010@mail.ru>
2022-02-25 09:54:28 +0600

    added file readme.md

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items: 3,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+
+
+
+    });
+});
